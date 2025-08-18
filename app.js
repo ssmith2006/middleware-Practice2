57ac553b302bc2106e553298e626a8f38e115219
ssmith2006 <shantel.smith@hotmail.com>
2025-08-18 03:45:56 +0000

    deleted and added routes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import bookRoutes from './routes/bookRoutes.js';
-import logger from './middlewares/logger.js'
-import authRoutes from "./routes/authRoutes.js";
+import express from "express";
+import bookRoutes from "./routes/bookRoutes.js";
+import logger from "./middlewares/logger.js";
+import borrowRoutes from "./routes/borrowRoutes.js";
+import authRoutes from ".//routes/authRoutes.js";
 
 const app = express();
-app.use(express.json());
-app.use(logger)
+app.use(express.json()); //1st middleware (app level middleware) receiving and sending using JSON
+app.use(logger);
 
-
-app.use('/', authRoutes);
-app.use('/', bookRoutes);
+app.use("/", borrowRoutes);
+app.use("/api", bookRoutes);
+app.use('/', authRoutes)
 
 export default app;
