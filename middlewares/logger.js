60b738249ab548157ce0f05e01344548d7199ea5
ssmith2006 <shantel.smith@hotmail.com>
2025-08-18 03:48:02 +0000

    modified syntax

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,3 +1,5 @@
-function logger(req, res, next){
-    console.log(`[${new Date().toISOString()}] ${req.method}-${req.url}`);
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method}-${req.url}`);
+  next();
 }
+export default logger;
