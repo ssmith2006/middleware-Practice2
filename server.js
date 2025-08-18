8941f1df29fce73df1a3662d2729f1196689d11a
ssmith2006 <shantel.smith@hotmail.com>
2025-08-18 03:46:47 +0000

    modified server syntax

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 import app from "./app.js";
-app.listen(3000, () => {console.log("Server running on port 3000");
+
+const PORT = 3000 //users will access API through PORT 3000
+
+app.listen(PORT, () => {console.log("Server running on port 3000");
 });
